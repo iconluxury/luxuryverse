7a01abf542c7d044a63c7bf9fb1c99e02040b003
nikwif <149531476+nikwif@users.noreply.github.com>
2025-04-15 17:47:18 +0000

    g

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.api.routes import collections, products
 from app.api.routes.x_auth import router as x_auth_router
-api_router = APIRouter(title="LuxuryVerse")
+api_router = APIRouter()
 
 api_router.include_router(collections.router, prefix="/collections", tags=["collections"])
 api_router.include_router(products.router, prefix="/products", tags=["products"])
