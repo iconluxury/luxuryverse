d06b2345b51468072072c45058f62e6b2d4082f1
nikwif <149531476+nikwif@users.noreply.github.com>
2025-04-16 23:33:21 +0000

    b

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from app.api.routes import utils, shopify
 from app.api.routes.x_auth import router as x_auth_router
+from app.core.config import settings
 api_router = APIRouter()
 
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(x_auth_router)
 api_router.include_router(shopify.shopify_router)
+api_router.include_router(api_router, prefix=settings.API_V1_STR)
