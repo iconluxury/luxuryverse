7b9d6290ad50f9b84fd0e5df784c389741793c03
nikwif <149531476+nikwif@users.noreply.github.com>
2025-04-16 14:35:13 +0000

    all

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,37 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react"; // Switch to esbuild
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import path from "path";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { visualizer } from "vite-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(),
     TanStackRouterVite({
       routesDirectory: "./src/routes",
-      generatedRouteTree: "./src/routeTree.gen.ts",
+      generatedRouteTree: "./src/routeTree.gen.ts"
     }),
     nodePolyfills({
-      globals: { Buffer: true, global: true },
+      globals: { Buffer: true, global: true }
     }),
+    visualizer() // For bundle analysis
   ],
   resolve: {
-    alias: { "@": path.resolve(__dirname, "src") },
+    alias: { "@": path.resolve(__dirname, "src") }
   },
   optimizeDeps: {
-    esbuildOptions: { define: { global: "globalThis" } },
+    esbuildOptions: { define: { global: "globalThis" } }
   },
+  build: {
+    sourcemap: false, // Disable sourcemaps for faster builds
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          wagmi: ["@wagmi/connectors", "viem"],
+          ox: ["ox"]
+        }
+      }
+    }
+  }
 });
