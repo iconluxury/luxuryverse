a8f637374db0f57dd29a74a259faa50568e0e187
nikwif <149531476+nikwif@users.noreply.github.com>
2025-04-16 23:34:33 +0000

    b

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter
 
-from app.api.routes import utils, shopify
+from app.api.routes import utils, shopify, private
 from app.api.routes.x_auth import router as x_auth_router
 from app.core.config import settings
 api_router = APIRouter()
 
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(x_auth_router)
-api_router.include_router(shopify.shopify_router)
-api_router.include_router(api_router, prefix=settings.API_V1_STR)
+api_router.include_router(shopify.shopify_router))
+
+
+# Private routes for local environment
+if settings.ENVIRONMENT == "local":
+    api_router.include_router(private.router)
