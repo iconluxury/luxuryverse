52880da25a4b2ae67a38dda979100853f3c06cb6
nikwif <149531476+nikwif@users.noreply.github.com>
2025-04-17 01:14:46 +0000

    base

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://iconluxury.today"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["GET", "OPTIONS"],
     allow_headers=["Accept"],
 )
