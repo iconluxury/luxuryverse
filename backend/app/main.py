366e12f7fd4936c56990c2618fab9cea0da055ab
nikwif <149531476+nikwif@users.noreply.github.com>
2025-04-17 15:24:11 +0000

    middle

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     allow_headers=["Accept", "Content-Type", "Authorization"],
 )
 
-app.include_router(api_router, prefix=settings.API_V_STR)
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 @app.get("/health", tags=["health"])
 async def health_check(request: Request):
