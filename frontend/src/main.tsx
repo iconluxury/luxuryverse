66057bbf2afbb1a90f43272054d968b730f8097e
nikwif <149531476+nikwif@users.noreply.github.com>
2025-04-17 22:47:30 +0000

    product-details

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { AppKitProvider } from '@reown/appkit/react';
 import ReactDOM from 'react-dom/client';
 import { routeTree } from './routeTree.gen';
 import { StrictMode } from 'react';
@@ -23,11 +22,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ChakraProvider theme={theme}>
       <WagmiProvider config={wagmiConfig}>
         <QueryClientProvider client={queryClient}>
-          <AppKitProvider>
-            <AuthProvider>
-              <RouterProvider router={router} />
-            </AuthProvider>
-          </AppKitProvider>
+          <AuthProvider>
+            <RouterProvider router={router} />
+          </AuthProvider>
         </QueryClientProvider>
       </WagmiProvider>
     </ChakraProvider>
