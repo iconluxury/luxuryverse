970fa1f56aab762d70fdd7562cbf4c2d38ede848
nikwif <149531476+nikwif@users.noreply.github.com>
2025-04-17 22:45:37 +0000

    product-details

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
-// src/index.tsx
+// src/main.tsx
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { AppKitProvider } from '@reown/appkit/react'; // Add this
+import { AppKitProvider } from '@reown/appkit/react';
 import ReactDOM from 'react-dom/client';
 import { routeTree } from './routeTree.gen';
 import { StrictMode } from 'react';
