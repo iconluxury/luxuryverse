81ca4ac365423c1082f93d77f3fd790b0efba61c
nikwif <149531476+nikwif@users.noreply.github.com>
2025-04-17 14:20:23 +0000

    middle

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from starlette.middleware.cors import CORSMiddleware
-
 from app.api.main import api_router
 from app.core.config import settings
 
@@ -12,17 +12,21 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
+    redirect_slashes=False,  
 )
+
+app.add_middleware(HTTPSRedirectMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://iconluxury.shop"],
     allow_credentials=False,
-    allow_methods=["GET", "OPTIONS","POST"],
-    allow_headers=["Accept","Content-Type", "Authorization"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Accept", "Content-Type", "Authorization"],
 )
+
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
-@app.get("/health", tags=["health"])  # Add tags here
+@app.get("/health", tags=["health"])
 async def health_check():
     return {"status": "healthy"}
