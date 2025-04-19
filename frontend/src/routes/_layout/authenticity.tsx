037e0a92e3e901d3c0a55aa06af75d2e1983a946
nik-macbookpro <nik@iconluxurygroup.com>
2025-04-19 10:36:36 -0400

    auth page

---
+++ frontend/src/routes/_layout/authenticity.tsx
@@ -0,0 +1,45 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { 
+  Box, 
+  Heading, 
+  Text, 
+  VStack, 
+  UnorderedList, 
+  ListItem, 
+  Container,
+  Divider, 
+  Link
+} from "@chakra-ui/react";
+import Footer from '../../components/Common/Footer';
+
+export const Route = createFileRoute('/_layout/authenticity')({
+  component: AuthenticityPage,
+});
+
+function AuthenticityPage() {
+  return (
+    <Box>
+      <Container maxW="1000px" mx="auto" px={4} py={16} color="gray.800">
+        <VStack spacing={8} align="start" w="full">
+          <Heading as="h1" size="xl" fontWeight="medium">
+       Authenticity
+          </Heading>
+          {/* Contact Information */}
+          <Box w="full" id="contact-information">
+          <Heading as="h2" size="lg" fontWeight="medium" mb={4}>
+              Contact
+            </Heading>
+            <Text fontSize="md" mb={4}>
+              If you have any questions, please reach out to us:
+            </Text>
+    
+            <Text><strong>Email:</strong> support@luxuryverse.com</Text>
+          </Box>
+        </VStack>
+      </Container>
+      <Footer />
+    </Box>
+  );
+}
+
+export default AuthenticityPage;
