3e9936d755f51b28f6cefbc258b9057080e1c171
nikwif <149531476+nikwif@users.noreply.github.com>
2025-04-21 20:20:26 +0000

    subtotal

--- frontend/src/routes/_layout/cart.tsx
+++ frontend/src/routes/_layout/cart.tsx
@@ -182,7 +182,7 @@ function Cart() {
                   </Text>
                 </Link>
                 <Text fontSize="md" fontWeight="bold" color="white">
-                    Subtotal: ${calculateSubtotal()}
+                  ${calculateSubtotal()}: SUBTOTAL
                   </Text>
               </HStack>
               <HStack justify="flex-end" w="100%" spacing={2}>
